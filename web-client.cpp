72bb10ef9d87917f3d8c20b45594c09b80ea06ba
anderson <andersonhuang95@gmail.com>
2016-04-23 14:58:07 -0700

    Just when I thought it worked...

--- web-client.cpp
+++ web-client.cpp
@@ -12,6 +12,7 @@
 #include <sstream>
 #include <cstdlib> 
 #include <netdb.h>
+#include <iterator> 
 #include "HTTP-message.h" 
 
 void error(const char *msg)
@@ -32,10 +33,12 @@ main(int argc, char **argv)
 	// Create a HTTP Request
 	std::string s(argv[1]); 
 	HttpRequest request(s);
+	/*
 	ByteBlob message = request.encode();  
 	std::string out(message.begin(), message.end());
 	std::cout << out;
 	std::cout << request.getPortNum() << std::endl; 
+	*/
 
 	// code for a client connecting to a server
 	// namely a stream socket to www.example.com on port 80 (http)
@@ -80,25 +83,77 @@ main(int argc, char **argv)
 	}
 
 	// Send and receive data 
-	char buf[8192];
+	ByteBlob response; 
 	ByteBlob copy_of_request = request.encode();
 	int size = copy_of_request.size();  
 	ret = write(sockfd, copy_of_request.data(), size); 
 	if (ret < 0)
 		perror("socket write failed");
 
+	// Get the filename 
+	std::string path = request.getUrl(); 
+	std::stringstream ss(path); 
+	std::string token, filename; 
+
+	while(std::getline(ss, token, '/')){
+		filename = token; 
+	}
 	// TODO: 
 	// Parse HTTPResponse
 	// Handle errors accordingly 
-	// Open a output file stream if successful and stuff data there
-	std::ofstream outfile("filename"); 
+	// Open a output file stream if successful and stuff data there 
+	char buf[8192];
+	memset(buf, 0, sizeof(buf)); 
 	while ( (ret = read(sockfd, buf, sizeof(buf))) != 0) {
+		// std::cout << ret << std::endl;
 		if (ret < 0)
 			error("socket read failed"); 
-		std::cout << buf;
+		response.insert(response.end(), buf, buf + strlen(buf));
 		memset(buf, 0, sizeof(buf)); 
 	}
 
+	// Parse reponse for HttpResponse Code
+	int index = 0; 
+	while(response[index] != ' '){
+		++index; 
+	}
+	++index; 
+	std::string status_code; 
+	while(response[index] != ' '){
+		status_code += response[index++]; 
+	}
+
+	std::cout << "HTTP request sent, awaiting response... "; 
+	// Deal with status code 
+	if(status_code == "200")
+		std::cout << "200 OK\n";
+	else if (status_code == "400")
+		std::cout << "400 Bad Request\n";
+	else if (status_code == "404")
+		std::cout << "404 Not Found\n";
+	else if (status_code == "403")
+		std::cout << "403 Forbidden\n";
+	else if (status_code == "501")
+		std::cout << "501 Not Implemented\n"; 
+	else if (status_code == "505")
+		std::cout << "505 HTTP version not supported\n";
+	else
+		std::cout << "Unknown error code encountered\n";
+
+	while(response[index] != '\r' || response[index + 1] != '\n' || response[index + 2] != '\r' || response[index + 3] != '\n')
+		index++;
+	index = index + 4; 
+	// Erase first (index) bytes
+	/*
+	for(int j = 0; j < response.size(); j++)
+		std::cout << response[j]; 
+	*/
+	
+	// response.erase(response.begin(), response.begin() + index); 
+	// Store remaining reponse into filename
+	std::ofstream outfile(filename);
+	std::ostream_iterator<uint8_t> oi(outfile, ""); 
+	std::copy(response.begin() + index, response.end(), oi); 
 	// free unneeded structures, close fds 
 	freeaddrinfo(servinfo); // all done with this structure
   	close(sockfd); 
